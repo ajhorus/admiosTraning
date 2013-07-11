dc17fbbad992943cee600f37c9369ee41e6bacf8
Roberto Duran <shaggyrd@gmail.com>
2013-07-11 15:46:40 -0500

    add frist 6 exercises of Seven Databases in Seven

---
+++ dataBasics/postgresql.sql
@@ -0,0 +1,40 @@
+/* Day 1: Relations, CRUD, and Joins */
+
+	-- 1. Select all tables we created (and only those) from pg_class.
+	SELECT * FROM pg_catalog.pg_tables WHERE tableowner = 'admios';
+
+	-- 2. Whrite a query that finds the country name of the “LARP Club” event.
+	SELECT country_name FROM events e
+	JOIN venues v ON e.venue_id = v.venue_id 
+	JOIN cities c ON v.postal_code=c.postal_code AND v.country_code=c.country_code 
+	JOIN countries co ON c.country_code = co.country_code
+	WHERE e.title= 'LARP Club';
+
+	-- 3. Alter the venues table to contain a boolean column called “active”, with the default value of TRUE
+	ALTER TABLE venues ADD COLUMN active boolean;
+	ALTER TABLE venues ALTER COLUMN active SET DEFAULT TRUE;
+
+/* Day 2: Advanced Queries, Code, and Rules */
+
+	-- 1. Create a rule that captures DELETEs on venues, and instead sets the active flag (created in Day 1 homework) to FALSE.
+	CREATE RULE delete_venues AS ON DELETE TO venues DO INSTEAD UPDATE venues SET active = FALSE WHERE venue_id = OLD.venue_id;
+
+	-- 2. A temporary table was not the best way to implement our event calendar
+    --    pivot table. The generate_series(a, b) function returns a set of records,
+	--    from a to b. Replace the month_count table select with this.
+	SELECT * FROM crosstab(
+	'SELECT extract(year from starts) as year,extract(month from starts) as month, count(*) FROM events GROUP BY year, month',
+	'SELECT * FROM generate_series(1,12)') AS (year int,jan int, feb int, mar int, apr int, may int, jun int,
+	jul int, aug int, sep int, oct int, nov int, dec int) ORDER BY YEAR;
+
+
+
+	-- 3. Build a pivot table that displays every day in a single month, where each
+	--    week of the month is a row, and each day name forms a column across
+	--    the top (seven days, starting with Sunday and ending with Saturday) like a
+	--    standard month calendar. Each day should contain a count of the number
+	--    of events that date, or remain blank if no event occurs.
+	SELECT * FROM crosstab(
+	'SELECT extract(month from starts) as month, 
+    div(extract (day from starts)::int, 7) + 1 as week, count(*) FROM events GROUP BY month, week ORDER BY month, week',
+	'SELECT * FROM generate_series(1, 5)') AS (month int,week_1 int, week_2 int, week_3 int, week_4 int, week_5 int) ORDER BY MONTH;

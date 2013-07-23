fa61189d06e8000c1074d86dddc1a9e9a14322b2
Roberto Duran <shaggyrd@gmail.com>
2013-07-23 11:35:06 -0500

    up

--- dataBasics/postgresql.sql
+++ dataBasics/postgresql.sql
@@ -43,4 +43,53 @@
 
 
 /* Day 3: Fulltext and Multidimensions */
+	--	Create a stored procedure where you can input a movie title or actor’s
+	--	name you like, and it will return the top 5 suggestions based on either
+	--	movies the actor has starred in, or films with similar genres.
+	CREATE OR REPLACE FUNCTION suggest_movies(search text)
+	RETURNS SETOF movies AS $$
+	DECLARE
+	  found_name text;
+	  found_type char(1);
+	  tempmovie movies%rowtype;
+	  movie_query tsquery;
+	BEGIN
+	  movie_query := to_tsquery(replace(regexp_replace(trim(search), ' +' , ' ', 'g'), ' ', '&'));
+	  SELECT INTO found_name, found_type
+	              name, type
+	  FROM (
+	    SELECT a.name AS name, 'A' AS type, levenshtein(lower(search), lower(a.name)) AS dist
+	      FROM actors a WHERE search % a.name
+	    UNION
+	  SELECT a.name AS name, 'A' AS type, levenshtein(lower(search), lower(a.name)) AS dist
+	        FROM actors a WHERE metaphone(a.name, 6) = metaphone(search, 6)
+	  UNION
+	  SELECT m.title AS name, 'M' AS type, levenshtein(lower(search), lower(m.title)) AS dist
+	      FROM movies m WHERE to_tsvector('english', m.title) @@ movie_query
+	       ) t
+	  ORDER BY dist LIMIT 1;
+
+	  IF found_type = 'A' THEN
+	    FOR tempmovie IN SELECT m.* FROM movies m NATURAL JOIN movies_actors NATURAL JOIN actors
+	                     WHERE name = found_name LIMIT 5 LOOP
+	      RETURN NEXT tempmovie;
+	    END LOOP;
+	  ELSE
+	    FOR tempmovie IN SELECT m.* FROM movies m,
+	                       (SELECT genre, title FROM movies WHERE title = found_name) s
+	                       WHERE cube_enlarge(s.genre, 5, 18) @> m.genre AND s.title <> m.title
+	                       ORDER BY cube_distance(m.genre, s.genre) LIMIT 5 LOOP
+	      RETURN NEXT tempmovie;
+	    END LOOP;
+	  END IF;
+	END;
+	$$ LANGUAGE plpgsql;
+
+	-- TEST	
+	SELECT title, genre FROM suggest_movies('Speed');
+
+
+	--	Expand the movies database to track user comments, and extract key-
+	--	words (minus english stopwords). Cross reference these keywords with
+	--	actor’s last names, and try and find the most talked about actors.
 	

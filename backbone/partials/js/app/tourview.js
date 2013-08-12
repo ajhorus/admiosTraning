6b6f34d438484ad9950c9f6219a78b93560149cf
Roberto Duran <shaggyrd@gmail.com>
2013-08-12 10:51:13 -0500

    delete unnecessary code

--- backbone/partials/js/app/tourview.js
+++ backbone/partials/js/app/tourview.js
@@ -10,10 +10,6 @@ App.Views.TourView = Backbone.View.extend({
     "<li><%= name %></li>"
   ),
 
-  initialize: function() {
-      this.collection.bind('reset', this.render, this);
-  },
-
   render: function() {
     var html = this.template({
       tours: this.collection.toJSON(),
